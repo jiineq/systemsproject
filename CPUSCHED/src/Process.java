b1bece11a7e580fa1e0ac948327066d2b9d1540a
Joy Zhang <jiineq@users.noreply.github.com>
2018-08-01 04:00:41 -0400

    might still have to do

--- CPUSCHED/src/Process.java
+++ CPUSCHED/src/Process.java
@@ -1,29 +1,15 @@
-import java.util.List;
-
-/**
- *https://www.geeksforgeeks.org/operating-system-priority-scheduling-different-arrival-time-set-2/
- *http://iips.icci.edu.iq/images/exam/Abraham-Silberschatz-Operating-System-Concepts---9th2012.12.pdf
- *https://www.geeksforgeeks.org/program-shortest-job-first-scheduling-set-2srtf-make-changesdoneplease-review/
- *https://www.youtube.com/watch?v=DvF3AsTglUU
- *https://github.com/search?q=cpu+scheduler+fifo
- *
- * https://github.com/ElvisGarcia079/Virtualization/blob/master/Scheduler.java
- * https://github.com/rrqq/cpu-scheduler
- *
- * runnable is for managing multiple threads
- *https://github.com/CormacAsGaeilge/CPU-Scheduler-Simulator
- * https://github.com/priyasjoshi/CPU-Scheduling
- * https://en.wikibooks.org/wiki/Operating_System_Design/Scheduling_Processes/FCFS
- * https://stackoverflow.com/questions/420223/what-is-the-difference-between-compare-and-compareto
- */
-
-
-
+//test 
 public class Process implements Comparable<Process> {
-    private int processID;
+	private int processID;
     private int arrivalTime;
     private int burstLength;
     private int priority;
+
+    public Process(int arrT, int burstT, int p) {
+        this.arrivalTime = arrT;
+        this.burstLength = burstT;
+        this.priority = p;
+    }
 
     public Process(int pid, int arrT, int burstT, int p) {
         this.processID = pid;
@@ -35,6 +21,7 @@ public class Process implements Comparable<Process> {
     public int getProcessID() {
         return processID;
     }
+
     public int getBurstLength() {
         return burstLength;
     }
@@ -47,6 +34,10 @@ public class Process implements Comparable<Process> {
         return priority;
     }
 
-
+    @Override
+    public int compareTo(Process p) {
+        return 1;
+    }
 
 }
+

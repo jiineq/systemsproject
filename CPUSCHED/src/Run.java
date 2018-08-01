34cf624e5e2be35d14d4e78f33d27bf10798a28c
Joy Zhang <jiineq@users.noreply.github.com>
2018-08-01 04:01:11 -0400

    todo cli stuff later

--- CPUSCHED/src/Run.java
+++ CPUSCHED/src/Run.java
@@ -1,112 +1,54 @@
-/**
- * Runs all the schedulers at user discretion and prints out statistics for the run
- *
- * command line format should be:
- *
- * java run -alg [FIFO||SJF||PR||RR]-quantum [integer] -inputFile [file.txt]
- * @author  Joy Zhang
- */
-
 import java.io.*;
-import java.util.HashMap;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
-public class Run {
-
-    public static void main (String[] args) throws InterruptedException, IOException {
-
-        Map<String, String> cliargs = new HashMap<>();
-        List<Process> log = new ArrayList<>();
-        String str = "";
-        File f;
-        Scanner scan;
-
-        for (String s:args) {
-            if(s.charAt(0) == '-') {
-                str = s;
-            }
-            else {
-                if (str.equals("")) {
-                        throw new RuntimeException("Invalid arguments");
-                }
-                cliargs.put(str, s); //str key, s value
-                str = "";
-                }
-            }
+public class thisisgay {
 
 
-        try {
-            f = new File(cliargs.get("-inputFile"));
-            scan = new Scanner(f);
-            String alg = cliargs.get("-alg");
-            int quantum = 0;
+	public static void main(String[] args) {
 
-            if (!(alg.equalsIgnoreCase("FIFO") || alg.equalsIgnoreCase("SJF") || alg.equalsIgnoreCase("PR") || (alg.equalsIgnoreCase("RR")))){
-                System.out.println("Argument invalid");
-            }
-            if (alg.equalsIgnoreCase("RR")) {
-                String quantumString = cliargs.getOrDefault("-quantum", "");
-                if (quantumString.equals("")) {
-                    throw new RuntimeException("Quantum is required");
-                }
-                quantum = Integer.parseInt(quantumString);
-            }
-
-        }
-
-        //TODO: MORE STUFF HERE
-
-        catch(FileNotFoundException e) {
-            System.out.println("File" + cliargs.get("-input") + "not found.");
-        }
+		File f = new File("input.txt");
+		//for testing 
+		List<Integer> records = new ArrayList<Integer>();
+		List<Process> jobs = new ArrayList<Process>();
+		int index = 0; 
+		int arrival; 
+		int burst; 
+		int priority;
 
 
+		try {
+			Scanner scan = new Scanner(f);
+			while (scan.hasNextLine()) {
+					String line = scan.nextLine();
+					//System.out.println(line);
+					Scanner scannedLine = new Scanner(line);
+					if (scannedLine.hasNextInt()) {
+						index = Integer.parseInt(scannedLine.next());
+						arrival = Integer.parseInt(scannedLine.next());
+						burst = Integer.parseInt(scannedLine.next());
+						priority = Integer.parseInt(scannedLine.next());
+						//records.add(burst);
+						Process p = new Process(index, arrival, burst, priority);
+						jobs.add(p);
+
+					}
+				
+			}
+			scan.close();
+
+		}
 
 
-        String fileout = cliargs.get("-input") + "Output.txt";
-        PrintWriter writer = new PrintWriter(fileout, "UTF-8");
-        writer.write("Statistics for the Run");
-        writer.write("Total elapsed time (for the scheduler): ");
-        writer.write("Throughput: ");
-        writer.write("Average waiting time: ");
-        writer.write("Average turnaround time: ");
-        writer.write("Average response time: ");
-        writer.close();
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.print("this doesn't work");
+		}
+		System.out.println(jobs);
+		//System.out.println(jobs.get(2).getProcessID());
+		//testing 
+		//System.out.println(records);
 
-
-    }
-
-    /**
-     * Calculates the statistics and prints for output file
-     * @param
-     */
-    private static void calculateStatistics(List<Process> log) throws IOException {
-
-        //TODO: CALCULATE STATS
-        double startT = log.get(0).arrT.get(0);
-        double[] arrT;
-
-
-        for (Process p: log) {
-
-            System.out.println("Statistics for the Run");
-            System.out.println("Total elapsed time (for the scheduler):") ;
-            System.out.println("Throughput: ");
-            System.out.println("CPU utilization: ");
-            System.out.println("Average waiting time: ");
-            System.out.println("Average turnaround time: ");
-            System.out.println("Average response time: ");
-
-        }
-
-
-
-
-
-    }
+	}
 
 }

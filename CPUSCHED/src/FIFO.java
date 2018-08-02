0bb2a4942967fe01f65444b3727004987d8c32c8
Joy <joy.zhang@engility.com>
2018-08-02 18:36:46 -0400

    finished fifo and i/o

---
+++ CPUSCHED/src/FIFO.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+/**
+ * FIFO/FCFS scheduler
+ * Non preemptive
+ */
+public class FIFO  {
+
+    private LinkedList<Process> jobs;
+
+    public FIFO(LinkedList<Process> joblist) {
+        this.jobs = joblist;
+    }
+
+    public int arrMin(LinkedList<Process> p) {
+        int min = p.get(0).getArrivalTime();
+        for (Process proc : p) {
+            if (proc.getArrivalTime() < min) {
+                min = proc.getArrivalTime();
+            }
+        }
+        return min;
+    }
+
+    public String calcFIFO() {
+        int wait = 0;
+        int turnaround = 0;
+        int arr = arrMin(jobs);
+
+        for (int i = 0; i < jobs.size(); i++) {
+            Process p = new Process(jobs.get(i).getArrivalTime(), jobs.get(i).getBurstLength(), jobs.get(i).getPriority());
+            arr += p.getBurstLength();
+            wait += (arr - p.getArrivalTime() - p.getBurstLength());
+            turnaround += (arr - p.getArrivalTime());
+        }
+
+
+        //TODO:
+        //System.out.println("Total elapsed time (for the scheduler):");
+        //System.out.println("Throughput: " + (double)jobs.size()/totaltime*60);
+        //System.out.println("CPU utilization: " );
+
+
+        String outp = "Statistics for the Run\n" + "Number of processes: " + jobs.size() + "\nTotal elapsed time (for the scheduler):\n" + "Throughput: \n" + "CPU utilization: " + "\nAverage waiting time: " + (double)wait/jobs.size() + "\nAverage turnaround time: " + (double)turnaround/jobs.size() + "\nAverage response time: "+ (double)wait/jobs.size();//+ //(double)wait/jobs.size();
+        return outp;
+
+    }
+
+
+}
--- CPUSCHED/src/Run.java
+++ CPUSCHED/src/Run.java
@@ -1,110 +1,111 @@
-/**
- * Runs all the schedulers at user discretion and prints out statistics for the run
- *
- * command line format should be:
- *
- * java run -alg [FIFO||SJF||PR||RR]-quantum [integer] -inputFile [file.txt]
- * @author  Joy Zhang
- */
-
 import java.io.*;
-import java.util.HashMap;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
+import java.util.HashMap;
 
 public class Run {
 
-    public static void main (String[] args) throws InterruptedException, IOException {
 
-        Map<String, String> cliargs = new HashMap<>();
-        List<Process> log = new ArrayList<>();
-        String str = "";
+    public static void main(String[] args) throws InterruptedException, IOException, FileNotFoundException {
+
+        String teststring = "this is a test string to check if things are working";
+
+        Map<String, String> map = new HashMap<>();
+        LinkedList<Process> jobs = new LinkedList<Process>();
+        int index;
+        int arrival;
+        int burst;
+        int priority;
+        String key = "";
         File f;
         Scanner scan;
+        String algorithm = "";
+        int quantum;
+        String save = "";
 
-        for (String s:args) {
-            if(s.charAt(0) == '-') {
-                str = s;
+        //collects from CLI for hashmap
+        //THIS WORKS!
+        for (String value:args) {
+            if (value.charAt(0) == '-') {
+                key = value;
+                // System.out.println(teststring);
+
             }
             else {
-                if (str.equals("")) {
-                        throw new RuntimeException("Invalid arguments");
+                if (key.equals("")) {
+                    throw new RuntimeException("Invalid arguments");
                 }
-                cliargs.put(str, s); //str key, s value
-                str = "";
-                }
+                map.put(key, value); //hashmap
+                key = "";
             }
-
+        }
 
         try {
-            f = new File(cliargs.get("-inputFile"));
+            f = new File(map.get("-input"));
             scan = new Scanner(f);
-            String alg = cliargs.get("-alg");
-            int quantum = 0;
+            algorithm = map.get("-alg");
 
-            if (!(alg.equalsIgnoreCase("FIFO") || alg.equalsIgnoreCase("SJF") || alg.equalsIgnoreCase("PR") || (alg.equalsIgnoreCase("RR")))){
-                System.out.println("Argument invalid");
-            }
-            if (alg.equalsIgnoreCase("RR")) {
-                String quantumString = cliargs.getOrDefault("-quantum", "");
+            if (algorithm.equalsIgnoreCase("RR")) {
+                String quantumString = map.getOrDefault("-quantum", "");
                 if (quantumString.equals("")) {
                     throw new RuntimeException("Quantum is required");
                 }
                 quantum = Integer.parseInt(quantumString);
             }
 
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                Scanner scannedLine = new Scanner(line);
+                if (scannedLine.hasNextInt()) {
+                    index = Integer.parseInt(scannedLine.next());
+                    arrival = Integer.parseInt(scannedLine.next());
+                    burst = Integer.parseInt(scannedLine.next());
+                    priority = Integer.parseInt(scannedLine.next());
+                    Process p = new Process(index, arrival, burst, priority);
+                    jobs.add(p);					}
+
+            }
+            scan.close();
+
         }
 
-        //TODO: MORE STUFF HERE
-
-        catch(FileNotFoundException e) {
-            System.out.println("File" + cliargs.get("-input") + "not found.");
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.print("this doesn't work");
         }
 
+        if (algorithm.equalsIgnoreCase("FIFO")) {
+            FIFO fifo = new FIFO(jobs);
+            save = fifo.calcFIFO();
+        }
 
+        //TODO
+        else if (algorithm.equalsIgnoreCase("SJF")) {
+            // SJF sjf = new SJF(jobs);
+            // sjf.calcSJF();
 
+        }
 
-        String fileout = cliargs.get("-input") + "Output.txt";
+        else if (algorithm.equalsIgnoreCase("PR")) {
+            // PR pr = new PR(jobs);
+            // pr.calcPR();
+        }
+
+        else if (algorithm.equalsIgnoreCase("RR")) {
+            // RR rr = new RR(jobs, quantum);
+            // rr.calcRR();
+        }
+
+		/*
+		 *  saves results in a .txt 
+		 */
+        String fileout = map.get("-input") + "Output.txt";
         PrintWriter writer = new PrintWriter(fileout, "UTF-8");
-        writer.write("Statistics for the Run");
-        writer.write("Total elapsed time (for the scheduler): ");
-        writer.write("Throughput: ");
-        writer.write("Average waiting time: ");
-        writer.write("Average turnaround time: ");
-        writer.write("Average response time: ");
+        writer.write(save);
+        writer.flush();
         writer.close();
 
-
-    }
-
-    /**
-     * Calculates the statistics and prints for output file
-     * @param
-     */
-    private static void calculateStatistics(List<Process> log) throws IOException {
-
-        //TODO: CALCULATE STATS
-        double startT = log.get(0).arrT.get(0);
-        double[] arrT;
-
-
-        for (Process p: log) {
-
-            System.out.println("Statistics for the Run");
-            System.out.println("Total elapsed time (for the scheduler):") ;
-            System.out.println("Throughput: ");
-            System.out.println("CPU utilization: ");
-            System.out.println("Average waiting time: ");
-            System.out.println("Average turnaround time: ");
-            System.out.println("Average response time: ");
-
-        }
-
-
-
+        System.out.println(save);
 
 
     }
